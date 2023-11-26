377e379666145dc7a6cb87c18a52ec8ab7c12f38
Aman1919 <amansingh33849@gmail.com>
2023-11-26 23:03:18 +0530

    changes

--- src/components/socket.tsx
+++ src/components/socket.tsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:1000/', {
+const socket = io('https://chess-game-server-xi.vercel.app/', {
         autoConnect: false
 });
 
